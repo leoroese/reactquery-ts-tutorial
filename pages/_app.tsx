bba5670e809c578b27cd51cfccc67c55e6ce6023
Leonardo Roese <leo.roese@hotmail.com>
2021-07-23 10:21:41 -0600

    finish project commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,32 @@
+/* eslint-disable react/jsx-props-no-spreading */
 // _app.tsx
 import '@styles/globals.css';
 
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 20 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 };
 
 export default MyApp;
